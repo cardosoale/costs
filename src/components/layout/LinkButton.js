53e18f62b6d8e7f4784e1dacfa691c806ef38a55
Alexandre Cardoso <112767802+cardosoale@users.noreply.github.com>
2023-08-25 11:49:34 -0300

    ..

---
+++ src/components/layout/LinkButton.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import styles from './LinkButton.module.css';
+
+
+function LinkButton({to, text}) {
+    return(
+        <Link className={styles.btn} to={to}>
+            {text}
+        </Link>
+    );
+}
+
+export default LinkButton;
--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,6 +1,36 @@
+import { useHistory } from 'react-router-dom';
+
+import ProjectForm from '../projects/ProjectForm';
+
+import styles from './NewProject.module.css';
+
 function NewProject() {
+    const history = useHistory();
+
+    function createPost(project) {
+        // initialize cost and services
+        project.cost = 0;
+        project.services = [];
+
+        fetch('http://localhost:5000/projects', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                history.push('/projects', { message: 'Projeto criado com sucesso!' });
+            });
+    }
+
     return (
-        <h1>NewProject</h1>
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
+        </div>
     );
 }
 
